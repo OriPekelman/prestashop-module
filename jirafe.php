e985d7ba6cdb94be4e59d6763b0bbc3aff9d8791
Scott Switzer <scott@switzer.org>
2011-09-20 14:18:43 -0400

    initial checkin

--- jirafe.php
+++ jirafe.php
@@ -1,120 +1,180 @@
 <?php
 
-// Set to true if you want to debug the Jirafe API
-define ('JIRAFE_DEBUG', false);
+if (!defined('_CAN_LOAD_FILES_'))
+	exit;
 
-// Include the other Jirafe files
-include_once 'PsApi.php';
-include_once 'JirafeApi.php';
-include_once 'PiwikTracker.php';
+// Set to true if you want to debug the Jirafe API in the test sandbox
+define ('JIRAFE_DEBUG', true);
 
 /*
-	REQUIREMENTS
-	
-	Use E_STRICT as error reporting level.
-	In php.ini:
-		- error_reporting = E_ALL | E_STRICT
-		- display_errors = On
-		
-	The name of the module folder and mail PHP file must be in lower case (Windows may not be case sensitive, but Unix based OS are).
-	Use the following folder architecture
-	- module folder
-		-> module file (same name than the module folder with .php at the end)
-		-> images folder
-		-> css folder
-		-> logo.gif (16x16, will appear in the module list of the back office)
-	Do not forget to translate your module.
-	Do not forget to remove hidden files (like .svn, .DS_store, .Thumbs) or temporary files (#file#, file~)
+    REQUIREMENTS
+    
+    Use E_STRICT as error reporting level.
+    In php.ini:
+        - error_reporting = E_ALL | E_STRICT
+        - display_errors = On
+        
+    The name of the module folder and mail PHP file must be in lower case (Windows may not be case sensitive, but Unix based OS are).
+    Use the following folder architecture
+    - module folder
+        -> module file (same name than the module folder with .php at the end)
+        -> images folder
+        -> css folder
+        -> logo.gif (16x16, will appear in the module list of the back office)
+    Do not forget to translate your module.
+    Do not forget to remove hidden files (like .svn, .DS_store, .Thumbs) or temporary files (#file#, file~)
 
-	If you want to use Ajax/Javascript, use firebug (a plugin for Firefox) to debug you script and check the compatibility with all browser.
-
+    If you want to use Ajax/Javascript, use firebug (a plugin for Firefox) to debug you script and check the compatibility with all browser.
 */
-
+    
 //Your class must have the same name than this file.
 class Jirafe extends Module
 {
-	public function __construct()
-	{
-		
-		//Name of your module. It must have the same name than the class
-		$this->name = 'jirafe';
-		
-		//You must choose an existing tab amongst the ones that are available
-		$this->tab = 'analytics_stats';
-		
-		//The version of your module. Do not forget to increment the version for each modification
-		$this->version = '1.0';
-
-		//The constructor must be called after the name has been set, but before you try to use any functions like $this->l()
-		parent::__construct();
-
-		//Name displayed in the module list
-		$this->displayName = $this->l('Jirafe');
-		
-		//Short description displayed in the module list
-		$this->description = $this->l('Integrated analytics for ecommerce managers');
-	}
+    // The Jirafe Client communicates with the Jirafe web service
+    private $jirafeClient;
+    // The Prestashop Client communicates with the Prestashop ecommerce platform
+    private $prestashopClient;
     
-	//You must implement the following methods if your module need to create a table, add configuration variables, or hook itself somewhere.
-	//-------------------------------
-	public function install()
-	{
+    public function __construct()
+    {
+        // Require/Autoload the other files
+        require_once _PS_MODULE_DIR_ . '/jirafe/vendor/jirafe-php-client/src/Jirafe/Autoloader.php';
+        require_once _PS_MODULE_DIR_ . '/jirafe/JirafeDashboardTab.php';
+        require_once _PS_MODULE_DIR_ . '/jirafe/PiwikTracker.php';
+        
+        Jirafe_Autoloader::register();
+        spl_autoload_register('__autoload');  // Re-register the default autoloader used by Prestashop (or ours will be the only autoloader)
+        
+        //Name of your module. It must have the same name than the class
+        $this->name = 'jirafe';
+        
+        //You must choose an existing tab amongst the ones that are available
+        $this->tab = 'analytics_stats';
+        $this->tabClassName = 'JirafeDashboardTab';
+        $this->tabParentName = ''; //in this example you add subtab under Tools tab. You may also declare new tab here
+        
+        //The version of your module. Do not forget to increment the version for each modification
+        $this->version = '1.1';
+        
+        //The constructor must be called after the name has been set, but before you try to use any functions like $this->l()
+        parent::__construct();
+        
+        //Name displayed in the module list
+        $this->displayName = $this->l('Jirafe Analytics');
+        
+        //Short description displayed in the module list
+        $this->description = $this->l('Integrated analytics for ecommerce managers');
+        
+        // Confirmation of uninstall
+        $this->confirmUninstall = $this->l('Are you sure you want to remove Jirafe analytics integration for your site?');
+        
+        // Prestashop Ecommerce Client
+        $ps = new Jirafe_Platform_Prestashop();
+        $this->prestashopClient = $ps;
+        
+        // Jirafe Client
+        if (JIRAFE_DEBUG) {
+            $ps->trackerUrl = 'test-data.jirafe.com';
+            $connection = new Jirafe_HttpConnection_Curl('https://test-api.jirafe.com/v1',443);
+            $this->jirafeClient = new Jirafe_Client($ps->get('token'), $connection);
+        } else {
+            $this->jirafeClient = new Jirafe_Client($ps->get('token'));
+        }
+    }
+    
+    //You must implement the following methods if your module need to create a table, add configuration variables, or hook itself somewhere.
+    //-------------------------------
+    public function install()
+    {
+        if (!$id_tab) {
+            $tab = new Tab();
+            $tab->class_name = $this->tabClassName;
+            $tab->id_parent = Tab::getIdFromClassName($this->tabParentName);
+            $tab->module = $this->name;
+            $languages = Language::getLanguages();
+            foreach ($languages as $language)
+                $tab->name[$language['id_lang']] = $this->displayName;
+            $tab->add();
+        }
+
+        $ps = $this->prestashopClient;
+        $jf = $this->jirafeClient;
+        
         // Get the application information needed by Jirafe
-        $app = PsApi::getApplication();
+        $app = $ps->getApplication();
 
         // Check if there is a token (probably not since we are installing) and if not, get one from Jirafe
-        if (empty($app['token'])) {
-
-            // Create the application in Jirafe
-            $app = JirafeApi::createApplication($app);
+        if (null === $app['token']) {
+            $app = $jf->applications()->create($app['name'], $app['url']);
             
             // Set the application information in Prestashop
-            PsApi::setApplication($app);
+            $ps->setApplication($app);
+            // Set the token in the Jirafe client for later
+            $jf->setToken($app['token']);
         }
         
         // Sync for the first time
-        PsApi::sync();
-        
+        $results = $jf->applications($app['app_id'])->resources()->sync($ps->getSites(), $ps->getUsers());
+            
+        // Save information back in Prestashop
+        $ps->setUsers($results['users']);
+        $ps->setSites($results['sites']);
+            
         // Add hooks for stats and tags
-		return (
+        return (
             parent::install()  // Get Jirafe ID, perform initial sync
             && $this->registerHook('backOfficeTop')  // Check to see if we should sync
-            && $this->registerHook('header')  // Install Jirafe tags
+            && $this->registerHook('header')         // Install Jirafe tags
+            && $this->registerHook('cart')           // When adding items to the cart
 //            && $this->registerHook('productfooter')  // Product specific information
 //            && $this->registerHook('home')  // Do we need this?
 //            && $this->registerHook('shoppingCartExtra')  // Shopping cart information
 //            && $this->registerHook('orderConfirmation'));  // Goal tracking
         );
-	}
+    }
     
-	public function uninstall()
-	{
-		if (!Configuration::deleteByName('JIRAFE_ID')
-            || !Configuration::deleteByName('JIRAFE_SITES')
-            || !Configuration::deleteByName('JIRAFE_SYNC')
-            || !Configuration::deleteByName('JIRAFE_TOKEN')
-            || !Configuration::deleteByName('JIRAFE_USERS')
-            || !parent::uninstall()) {
-			return false;
+    public function viewAccess($disable = false) {
+            $result = true;
+            return $result;
+    }
+
+    public function uninstall()
+    {
+        $id_tab = Tab::getIdFromClassName($this->tabClassName);
+        if ($id_tab) {
+            $tab = new Tab($id_tab);
+            $tab->delete();
         }
-		return true;
-	}
-	//-------------------------------
-	
-	//Display Configuration page of your module.
-	public function getContent()
-	{
-		global $currentIndex;
-		
+        
+        $ps = $this->prestashopClient;
+        
+        if (!$ps->delete('app_id')
+            || !$ps->delete('sites')
+            || !$ps->delete('users')
+            || !$ps->delete('sync')
+            || !$ps->delete('token')
+            || !parent::uninstall()) {
+            return false;
+        }
+        return true;
+    }
+    
+    //-------------------------------
+    
+    //Display Configuration page of your module.
+    public function getContent()
+    {
+        global $currentIndex;
+        
         $html = '<h2>'.$this->displayName.'</h2>';
 
-		if (Tools::isSubmit('submitJirafe')) {
-			Configuration::updateValue('JIRAFE_SITE_ID', (int)Tools::getValue('JIRAFE_SITE_ID'));
-			$html .= $this->displayConfirmation($this->l('Configuration updated'));
-		}
+        if (Tools::isSubmit('submitJirafe')) {
+                Configuration::updateValue('JIRAFE_SITE_ID', (int)Tools::getValue('JIRAFE_SITE_ID'));
+                $html .= $this->displayConfirmation($this->l('Configuration updated'));
+        }
 		
-		$conflink = $currentIndex.'&configure='.$this->name.'&token='.Tools::getValue('token');
-		$html .= '
+        $conflink = $currentIndex.'&configure='.$this->name.'&token='.Tools::getValue('token');
+        $html .= '
 		<fieldset><legend>'.$this->l('Configuration').'</legend>
 			<form action="'.htmlentities($conflink).'" method="post">
 				<label>'.$this->l('Site ID').'</label>
@@ -126,8 +186,8 @@ class Jirafe extends Module
         	</form>
 		</fieldset>
 		<div class="clear">&nbsp;</div>';
-		return $html;
-	}
+        return $html;
+    }
 
     /**
      * Check to see if someone saved something we need to update Jirafe about
@@ -137,14 +197,24 @@ class Jirafe extends Module
      */
     public function hookBackOfficeTop($params)
     {
+        $ps = $this->prestashopClient;
+        
         // Back Office Top hook is called twice - once before saving, and once after.  So, when we initially come here, we have not saved yet.
         //  We just set a flag.  The second time we come here, we have already saved, and so we check the flag and sync.
-        if (Configuration::get('JIRAFE_SYNC')) {
-            Configuration::updateValue('JIRAFE_SYNC', false);
-            PsApi::sync();
+        
+        if ($ps->get('sync')) {
+            $ps->set('sync', false);
+            
+            // Sync the changes
+            $app = $ps->getApplication();
+            $results = $this->jirafeClient->applications($ps->get('app_id'))->resources()->sync($ps->getSites(), $ps->getUsers());
+            
+            // Save information back in Prestashop
+            $ps->setUsers($results['users']);
+            $ps->setSites($results['sites']);
         }
-        if (PsApi::checkSync($params)) {
-            Configuration::updateValue('JIRAFE_SYNC', true);
+        if ($ps->isDataChanged($params)) {
+            $ps->set('sync', true);
         }
     }
     
@@ -154,36 +224,31 @@ class Jirafe extends Module
      * @param array $params variables from the front end
      * @return string the additional HTML that we are generating in the header
      */
-	public function hookHeader($params)
-	{
-        // Set some variables needed to generate the ad tag
-        $siteId = PsApi::getSiteId();
-        $visitorType = PsApi::getVisitorType($siteId);
-        
-        // Get the HTML
-        $html = '
-    <script type="text/javascript">
-    var _paq = _paq || [];
-    (function(){
-        var u=(("https:" == document.location.protocol) ? "https://data.jirafe.com/" : "http://data.jirafe.com/");
-        _paq.push([\'setSiteId\', '.$siteId.']);
-        _paq.push([\'setTrackerUrl\', u+\'piwik.php\']);
-        _paq.push([\'enableLinkTracking\']);
-        _paq.push([\'setCustomVariable\',\'1\',\'U\',\''.$visitorType.'\']);
-        _paq.push([\'trackPageView\']);
-        
-        var d=document,
-            g=d.createElement(\'script\'),
-            s=d.getElementsByTagName(\'script\')[0];
-            g.type=\'text/javascript\';
-            g.defer=true;
-            g.async=true;
-            g.src=u+\'piwik.js\';
-            s.parentNode.insertBefore(g,s);
-    })();
-    </script>';
+    public function hookHeader($params)
+    {
+        $ps = $this->prestashopClient;
+        return $ps->getTag();
+    }
     
-        return $html;
+    /**
+     * Hook which gets called when a user adds something to their cart.
+     * We then send Jirafe the updated cart information
+     *
+     * @param array $params variables from the front end
+     */
+    public function hookCart($params)
+    {
+        // Get the ecommerce client
+        $ps = $this->prestashopClient;
+        
+        // First get the details of the cart to log to the server
+        $cart = $ps->getCart($params);
+
+        // Then get the details of the visitor
+        //$visitor = $ps->getVisitor($params);
+        
+        // Log the cart update for this visitor
+        $ps->logCartUpdate($cart);
     }
 }
 
