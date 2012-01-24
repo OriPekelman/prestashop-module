6dc962d4901eb6403bcd0f527a72fe955cf494e0
Vincent Velociter <vincent.velociter@gmail.com>
2012-01-24 12:58:53 +0100

    Add locale to jirafe constructor

--- AdminJirafeDashboard.php
+++ AdminJirafeDashboard.php
@@ -9,27 +9,28 @@ class AdminJirafeDashboard extends AdminTab
         parent::__construct();
     }
 
-	public function display()
-	{
+    public function display()
+    {
 /*		global $cookie;
-		$this->warnDomainName();
+        $this->warnDomainName();
 
-		$tab = get_class();
-		$protocol = (!empty($_SERVER['HTTPS']) AND strtolower($_SERVER['HTTPS']) != 'off')?'https':'http';
-		$isoDefault = Language::getIsoById(intval(Configuration::get('PS_LANG_DEFAULT')));
-		$isoUser = Language::getIsoById(intval($cookie->id_lang));
-		$isoCountry = Country::getIsoById(Configuration::get('PS_COUNTRY_DEFAULT'));
-		$currency = new Currency((int)(Configuration::get('PS_CURRENCY_DEFAULT')));
-*/
-	        $jirafe = new Jirafe();
+        $tab = get_class();
+        $protocol = (!empty($_SERVER['HTTPS']) AND strtolower($_SERVER['HTTPS']) != 'off')?'https':'http';
+        $isoDefault = Language::getIsoById(intval(Configuration::get('PS_LANG_DEFAULT')));
+        $isoUser = Language::getIsoById(intval($cookie->id_lang));
+        $isoCountry = Country::getIsoById(Configuration::get('PS_COUNTRY_DEFAULT'));
+        $currency = new Currency((int)(Configuration::get('PS_CURRENCY_DEFAULT')));
+ */
+        $jirafe = new Jirafe();
+        $ps = $jirafe->getPrestashopClient();
 
-		$apiUrl = (JIRAFE_DEBUG) ? 'https://test-api.jirafe.com/v1' : 'https://api.jirafe.com/v1';
-                $ps = $jirafe->getPrestashopClient();
-                $token = $ps->get('token');
-                $appId = $ps->get('app_id');
-                $title = $this->l('Dashboard');
-                $errMsg = $this->l("We're unable to connect with the Jirafe service for the moment. Please wait a few minutes and refresh this page later.");
-                echo <<<EOF
+        $apiUrl = (JIRAFE_DEBUG) ? 'https://test-api.jirafe.com/v1' : 'https://api.jirafe.com/v1';
+        $token = $ps->get('token');
+        $appId = $ps->get('app_id');
+        $locale = $ps->getLanguage();
+        $title = $this->l('Dashboard');
+        $errMsg = $this->l("We're unable to connect with the Jirafe service for the moment. Please wait a few minutes and refresh this page later.");
+        echo <<<EOF
 <div>
     <h1>{$title}</h1>
     <hr style="background-color: #812143;color: #812143;" />
@@ -46,6 +47,7 @@ class AdminJirafeDashboard extends AdminTab
             api_url:    '{$apiUrl}',
             api_token:  '{$token}',
             app_id:     '{$appId}',
+            locale:     '{$locale}',
             version:    'presta-v0.1.0'
          });
     })(jirafe.jQuery);
@@ -61,5 +63,5 @@ setTimeout(function() {
 <!-- Jirafe Dashboard End -->
 EOF;
 
-	}
+    }
 }
