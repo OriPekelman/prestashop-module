495e5a3b0262a2702248f36a07dce5688504443d
Vincent Velociter <vincent.velociter@gmail.com>
2012-07-10 17:07:24 +0200

    Fix tracker url

--- jirafe.php
+++ jirafe.php
@@ -12,7 +12,7 @@ define('JIRAFE_MODULE_VERSION', '0.1');
 // Urls configuration
 define('JIRAFE_API_URL', (JIRAFE_DEBUG) ? 'https://test-api.jirafe.com/v1' : 'https://api.jirafe.com/v1');
 define('JIRAFE_API_PORT', 443);
-define('JIRAFE_TRACKER_URL', (JIRAFE_DEBUG) ? 'data.jirafe.com' : 'test-data.jirafe.com');
+define('JIRAFE_TRACKER_URL', (JIRAFE_DEBUG) ? 'test-data.jirafe.com' : 'data.jirafe.com');
 define('JIRAFE_ASSETS_URL_PREFIX', 'https://jirafe.com/dashboard'); // without trailing slash
 
 require_once 'jirafe_base.php';
