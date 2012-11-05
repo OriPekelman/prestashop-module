0eaac8804b659ded0f757dfabb1058e59dbe6f05
Vincent Velociter <vincent.velociter@gmail.com>
2012-11-05 16:59:03 +0100

    Use php-client master; move prestashop specific here

--- jirafe.php
+++ jirafe.php
@@ -10,12 +10,16 @@ define('JIRAFE_DEBUG', true);
 define('JIRAFE_MODULE_VERSION', '0.1');
 
 // Urls configuration
-define('JIRAFE_API_URL', (JIRAFE_DEBUG) ? 'https://test-api.jirafe.com/v1' : 'https://api.jirafe.com/v1');
-define('JIRAFE_API_PORT', 443);
+define('JIRAFE_API_URL', (JIRAFE_DEBUG) ? 'http://api.jirafe.local/v1' : 'https://api.jirafe.com/v1');
+define('JIRAFE_API_PORT', 80);
 define('JIRAFE_TRACKER_URL', (JIRAFE_DEBUG) ? 'test-data.jirafe.com' : 'data.jirafe.com');
-define('JIRAFE_ASSETS_URL_PREFIX', 'https://jirafe.com/dashboard'); // without trailing slash
+define('JIRAFE_ASSETS_URL_PREFIX', 'http://en.jirafe.local/dashboard'); // without trailing slash
 
 require_once 'jirafe_base.php';
+require_once 'Platform/Interface.php';
+require_once 'Platform/Ecommerce.php';
+require_once 'Platform/Prestashop14.php';
+require_once 'Platform/Prestashop15.php';
 
 if (version_compare(_PS_VERSION_, '1.5') >= 0) {
     require_once 'jirafe15.php';
